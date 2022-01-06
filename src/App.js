5fb052ac2d1dfeed11d1b99502cd7d547b33d451
RolandYambao <rolandyambao@gmail.com>
2022-01-05 20:50:28 -0800

    Too complicated, instead start from the MERN Frontend piece and transform it into Mercer

--- src/App.js
+++ src/App.js
@@ -1,35 +1,76 @@
-import React, { Component } from "react";
+import React, { useEffect, useState, Component } from 'react';
 import {
   BrowserRouter as Router,
   Route,
   Routes,
+  Switch,
+  Redirect,
 } from 'react-router-dom';
+import jwt_decode from 'jwt-decode';
+import setAuthToken from './utils/setAuthToken';
 
-// components
+// Components
 import Login from "./components/login/Login";
 import Register from "./components/register/Register";
 import CardContainer from "./components/cards/CardContainer";
 import HomeContainer from "./components/home/HomeContainer";
 import Item from "./components/item/item";
 
+const PrivateRoute = ({ component: Component, ...rest }) => {
+  let token = localStorage.getItem('jwtToken');
+  // console.log('===> Hitting a Private Route');
+  return <Route {...rest} render={(props) => {
+    return token ? <Component {...rest} {...props} /> : <Redirect to="/login" />
+  }} />
+}
 
-class App extends Component {
-  render() {
-    return (
-     <Router>
-       <div>
+function App() {
+  // Set state values
+  const [currentUser, setCurrentUser] = useState('');
+  const [isAuthenticated, setIsAuthenticated] = useState(true);
+
+  useEffect(() => {
+    let token;
+
+    if (!localStorage.getItem('jwtToken')) {
+      setIsAuthenticated(false);
+      // console.log('====> Authenticated is now FALSE');
+    } else {
+      token = jwt_decode(localStorage.getItem('jwtToken'));
+      setAuthToken(localStorage.getItem('jwtToken'));
+      setCurrentUser(token);
+    }
+  }, []);
+
+  const nowCurrentUser = (userData) => {
+    // console.log('===> nowCurrentUser is here.');
+    setCurrentUser(userData);
+    setIsAuthenticated(true);
+  }
+
+  const handleLogout = () => {
+    if (localStorage.getItem('jwtToken')) {
+      // remove token for localStorage
+      localStorage.removeItem('jwtToken');
+      setCurrentUser(null);
+      setIsAuthenticated(false);
+    }
+  }
+
+  return (
+    <Router>
+      <div>
         <Routes>
           <Route exact path="/" element={<HomeContainer />} />
-          <Route path="/login" element={<Login />}/>
-          <Route path="/register" element={<Register />}/>
-          <Route path="/card" element={<CardContainer />}/>
-          <Route path="/home" element={<HomeContainer />}/>
-          <Route path="/item" element={<Item />}/>
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/card" element={<CardContainer />} />
+          <Route path="/home" element={<HomeContainer />} />
+          <Route path="/item" element={<Item />} />
         </Routes>
-       </div>
-     </Router>
-    );
-  }
+      </div>
+    </Router>
+  );
 }
 
 export default App;
